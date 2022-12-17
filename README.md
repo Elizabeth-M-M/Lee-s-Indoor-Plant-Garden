d65d8e29b48d12a5766f68d247fe64000aa23653
Elizabeth <mwendemuthusi6@gmail.com>
2022-12-17 07:36:25 +0300

    Added content to the README.md file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Lee-s-Indoor-Plant-Garden
+# Lee's Indoor Garden Shop
+####Catering for your indoor gardening needs
+### Owner
+Program by Elizabeth Mwende Muthusi
+### Date
+15/12/2022.
+
+### About the program
+This is a mini web app/ page belonging to a person who sells indoor house plants. A user is able to get a vision of what he/ she wants to plant and select from a wide variety of categories depending on the user's needs.
+If a user selects a plant from a particular category by clicking `More`, a pop up of the plant details will pop up showing different requirements needed to grow the plant, fun facts about its origin. If the user is interested, he will click the button `Interested`.
+A pop up page again will then appear prompting the user to enter details pertaining to name, height of plant required, live or artificial, delivery location and telephone number. The user clicks on submit to submit the data.
+If the user is not interested, they can close the pop up and continue browsing.
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+// imported api key from environment.js
 import keyForUse from "./environment.js";
-
 let currentItem = 0;
+let storageForContainers = document.querySelector('.most-liked-width.container');
 
+// When DOM loads, start of the page carousel works with the buttons and plants are loaded
 document.addEventListener('DOMContentLoaded', ()=>{
  loadPlantsFromExternalServer();
  getDataForCarousel()
- 
+
 })
+// fetch half of data from external API
 function loadPlantsFromExternalServer(){
     const options = {
       method: "GET",
@@ -18,34 +21,29 @@ function loadPlantsFromExternalServer(){
     fetch("https://house-plants.p.rapidapi.com/all", options)
       .then((response) => response.json())
       .then((responses) => {
-        const half = Math.ceil(responses.length / 2); 
-        const firstHalf = responses.slice(0, half);        
+        const half = Math.ceil(responses.length / 2);
+        const firstHalf = responses.slice(0, half);
         firstHalf.forEach(response=>{
-          // console.log(response.category)
           displayPlantinCategory(response)
         })
       })
       .catch((err) => console.error(err));
 }
-
-
-
+// Plants are displayed according to the category they fall under using click event listener
 function displayPlantinCategory(plant){
   const btns = document.querySelectorAll('.plantBtn');
   btns.forEach(btn=>{
     btn.addEventListener('click', (event)=>{
       if(btn.innerHTML === plant.category){
         if(plant.common[0]!==undefined){
-          displayPlants(plant);        
+          displayPlants(plant);
         }
       }
     })
   })
 }
-
-function displayPlants(plant){
-    // console.log(plant)
-    let storageForContainers = document.querySelector('.most-liked-width.container');
+// Display plants
+function displayPlants(plant){    
     let container = document.createElement('div');
     container.className=`container-card-holder withoutDits ${plant.category}`
     container.innerHTML=`
@@ -98,31 +96,22 @@ function displayPlants(plant){
   `
   storageForContainers.appendChild(container);
   // remove kids of container that do not belong to the plant.category
-  fetchImagesInternally()
-  
-
-
   let kids = storageForContainers.children;
   removeUnnecesary(kids, plant.category)
 }
+// The external api does not have image sources, so images are fetched from the internal api
 function fetchImagesInternally(){
   fetch('http://localhost:3000/plantPics')
   .then(resp=>resp.json())
   .then(greeneries=>greeneries.forEach(greenery=>{
-  
   let cards = document.querySelectorAll('.container-card.withoutDits')
   // console.log(cards)
   cards.forEach(card=>{
     let title= card.querySelector('#plantName');
     if(title.innerHTML=== greenery.name){
-      let image= title.previousElementSibling
-      // console.log(greenery.img)
-      // console.log(image.src)
+      let image= title.previousElementSibling;      
       image.src=greenery.img;
     }
-    
-    
-  
   })
   }))
 }
@@ -134,11 +123,12 @@ function removeUnnecesary(plants, category){
     if(plant.classList[2]!==category){
       plant.remove()
     }
+    fetchImagesInternally()
     let moreBtns=document.querySelectorAll('#moreBtn')
     // console.log(moreBtns)
     moreBtns.forEach(more=>{
       more.addEventListener('click', ()=>{
-        console.log(more.parentElement)
+        // console.log(more.parentElement)
         let containerCards = document.querySelectorAll('.container-card')
         let parent1 =more.parentElement;
         let grandparent = parent1.parentElement;
@@ -167,11 +157,19 @@ function removeUnnecesary(plants, category){
             let modalChild = btn.parentElement
             let modal = modalChild.parentElement;
             let overlay = modal.nextElementSibling;
-            console.log(modal.classList)
-            console.log(overlay)
+            let detailsDiv =modal.previousElementSibling;
+            let containerCard = detailsDiv.previousElementSibling;
+            let parent = detailsDiv.parentElement
+
+            // console.log(modal.classList)
+            // console.log(overlay)
 
             modal.classList.remove("active");
             overlay.classList.remove("active")
+            detailsDiv.classList.add('invisible')
+            containerCard.classList.add('withoutDits')
+            parent.classList.add('withoutDits')
+
         })
         })
 
@@ -184,7 +182,7 @@ function removeUnnecesary(plants, category){
           if(cardSibling.classList.contains('invisible')){
           }else{
             if(title!==titleCard){
-            console.log(title)
+            // console.log(title)
             cardSibling.classList.add('invisible')
             card.classList.add('withoutDits')
             cardParent.classList.add('withoutDits')
@@ -219,21 +217,24 @@ function collectDeliveryInfo(form){
     let fullname= form.querySelector('#fullname').value;
     let plantType= form.querySelector('#artificialOrLive').value;
     // console.log(tel, delivery, height, fullname, plantType)
-    postData(tel, delivery, height, fullname, plantType)
-}
-function postData(tel, delivery, height, fullname, plantType){
-  fetch('http://localhost:3000/orders',{
-    method: 'POST',
-    headers:{
-      'Content-Type':"application/json",
-    },
-    body:JSON.stringify({
+    let data = {
       "name": fullname,
       "plantType":plantType,
       "height":height,
       "delivery": delivery,
       "telephoneNumber":tel,
-    })
+    }
+    // postData(data)
+}
+// Post is not working; To resolve
+function postData(data){
+
+  fetch('http://localhost:3000/orders',{
+    method: 'POST',
+    headers:{
+      'Content-Type':"application/json",
+    },
+    body:JSON.stringify(data)
   }).then(response=>response.json())
   .then(data=>console.log(data))
 }
@@ -248,13 +249,13 @@ fetch(`http://localhost:3000/startSection`)
 .then(resp=>resp.json())
 .then(responses=>{
   displayCourosel(responses)
-  
+
 })
 }
 function displayCourosel(responses){
-  
+
   if(currentItem>responses.length-1){
-    currentItem=0; 
+    currentItem=0;
   }else if(currentItem<0){
 currentItem=responses.length-1
   }
@@ -262,15 +263,15 @@ currentItem=responses.length-1
   header.innerHTML=item.title
   headerP.innerHTML=item.text
   headerImage.src=item.image
- 
+
 }
 right.addEventListener('click', ()=>{
-  
+
 currentItem++
   // console.log(currentItem)
   getDataForCarousel();
- 
-  
+
+
 })
 left.addEventListener('click', ()=>{
   currentItem--
