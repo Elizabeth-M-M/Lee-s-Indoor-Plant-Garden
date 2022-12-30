f8cc16b591f978d7bf2158541736c169e418f10d
Elizabeth <mwendemuthusi6@gmail.com>
2022-12-30 23:10:07 +0300

    modified js to be easily understood

--- index.js
+++ index.js
@@ -1,235 +1,227 @@
+
 // imported api key from environment.js
 import keyForUse from "./environment.js";
-let currentItem = 0;
-let storageForContainers = document.querySelector('.most-liked-width.container');
-
-// When DOM loads, start of the page carousel works with the buttons and plants are loaded
+const plantBtns = document.querySelectorAll('.plant-btn');
+const container = document.querySelector('.plantBoxHolder')
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': keyForUse,
+		'X-RapidAPI-Host': 'house-plants.p.rapidapi.com'
+	}
+};
 document.addEventListener('DOMContentLoaded', ()=>{
- loadPlantsFromExternalServer();
- getDataForCarousel()
-
+  retrievePlants();
 })
-// fetch half of data from external API
-function loadPlantsFromExternalServer(){
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key":keyForUse,
-        "X-RapidAPI-Host": "house-plants.p.rapidapi.com",
-      },
-    };
-    fetch("https://house-plants.p.rapidapi.com/all", options)
-      .then((response) => response.json())
-      .then((responses) => {
-        const half = Math.ceil(responses.length / 2);
-        const firstHalf = responses.slice(0, half);
-        firstHalf.forEach(response=>{
-          displayPlantinCategory(response)
-        })
-      })
-      .catch((err) => console.error(err));
+// fetch plants from the external API
+function retrievePlants(){
+fetch('https://house-plants.p.rapidapi.com/all', options)
+	.then(response => response.json())
+	.then(plants => {
+    useOnlyHalfOfplants(plants)
+  })
+	.catch(err => console.error(err));
 }
-// Plants are displayed according to the category they fall under using click event listener
-function displayPlantinCategory(plant){
-  const btns = document.querySelectorAll('.plantBtn');
+// retrieve half of plants(since this API does not have images)
+function useOnlyHalfOfplants(plants){
+  let plantsCollected = []
+  let half =plants.length/2
+  plants.forEach(plant=>{
+    if(plant.id<=half){
+      plantsCollected.push(plant)
+    }
+  })
+  // For the retrieved plants (half), in th event a button of specific categories are clicked, it calls the displayPlantInCategory() passing the plants and specific category of button (property)
+  plantBtns.forEach(btn=>
+    {
+      btn.addEventListener('click', (event)=>{
+        let property = btn.innerHTML;
+        displayPlantInCategory(plantsCollected, property);
+      })
+    })
+}
+// to display selected plants in category to the DOM
+function displayPlantInCategory(plants, property){
+  plants.forEach(plant=>{
+    // When a button of a particular category is clicked, only plants belonging to that category are collected
+    if(plant.category===property){
+      let box= document.createElement('div');
+      box.classList= `plantBox col-lg-3 col-md-4 col-sm-6 ${plant.category}`
+      box.innerHTML=`
+        <div class="boxCard card">
+          <div class="card-body">
+            <img src="" class="img-fluid pb-2" alt="">
+            <h4 id="plantName" class="card-title">${plant.common[0]}</h4>
+            <a class="btn btn-success" id="moreBtn" role="button">More</a>
+          </div>
+        </div>
+        <div class="modals modalMore bg-light">
+          <h4 class="text-center">Taking care of<span class="contrast-color"> ${plant.common[0]}</span></h4>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Watering: <span class="contrast-color">${plant.watering}</span></li>
+            <li class="list-group-item">Ideal light: <span class="contrast-color">${plant.ideallight}</span></li>
+            <li class="list-group-item">Origin: <span class="contrast-color">${plant.origin}</span></li>
+            <li class="list-group-item">Use: <span class="contrast-color">${plant.use[0]}</span></li>
+          </ul>
+          <button class="btn btn-success w-100" id="interestedBtn">Interested</button>
+        </div>
+        <div class="modals modalInterested bg-light" id="modal">
+          <div class="modal-title">
+            <h3><span class="contrast-color">${plant.common[0]}</span> is a great pick</h3>
+            <button class="contrast-color ms-3" id="closeModal">&times;</button>
+          </div>
+          <form class="text-start">
+            <div >
+              <label for="name" class="col-form-label">Name</label>
+              <input type="text" name="fullname" id="fullname" class="form-control" placeholder="Your name here">
+            </div>
+            <div>
+              <label for="plant type" class="col-form-label">Which plant type do you need?</label><br>
+              <input type="text" name="artificial/live" id="artificialOrLive" class="form-control" placeholder="Artificial/Live">
+            </div>
+            <div>
+              <label for="height" class="col-form-label">Approximate height needed</label>
+              <input type="text" name="height" id="height" class="form-control" placeholder="In cm">
+            </div>
+            <div >
+              <label for="location" class="col-form-label">Location</label>
+              <input type="text" placeholder="Delivery location" class="form-control" id="delivery">
+            </div>
+            <div >
+              <label for="contact" class="col-form-label">Telephone Number</label>
+              <input type="tel" name="telephone" placeholder="+254..." class="form-control" id="telephone">
+            </div>
+            <div class="mt-3">
+              <a class="btn btn-success" id="backBtn" role="button">Back</a>
+              <button class="btn btn-success" type="submit" id="orderBtn">Order now</button>
+            </div>
+          </form>
+        </div>
+        <div id="overlay"></div>
+      `
+      container.appendChild(box);
+    }
+  })
+  // explanations for these functions are given above each function
+  removePlantsOfNotSameCategory(property);
+  respectiveImage();
+  findPair();
+  removeUndefinedTitles();
+  // This targeted the more button to show more details
+  let btns = document.querySelectorAll('#moreBtn')
   btns.forEach(btn=>{
-    btn.addEventListener('click', (event)=>{
-      if(btn.innerHTML === plant.category){
-        if(plant.common[0]!==undefined){
-          displayPlants(plant);
-        }
-      }
+    btn.addEventListener('click',()=>{
+      moreDetails(btn);
     })
   })
 }
-// Display plants
-function displayPlants(plant){    
-    let container = document.createElement('div');
-    container.className=`container-card-holder withoutDits ${plant.category}`
-    container.innerHTML=`
-          <div class="container-card withoutDits">
-            <img src="" alt="">
-            <h3 id="plantName">${plant.common[0]}</h3>
-            <button id="moreBtn" class="btn">More</button>
-          </div>
-          <div class="container-card-details invisible">
-            <h2>Need to know about <span> ${plant.common[0]}</span></h2>
-            <h4>watering</h4>
-            <p>${plant.watering}</p>
-            <h4>Ideal light</h4>
-            <p>${plant.ideallight}</p>
-            <h4>Origin: <span>${plant.origin}</span></h4>
-            <h4>Use: <span>${plant.use[0]}</span></h4>
-            <button class="btn btn-dark" data-modal-target="#modal" id="openModal">Interested</button>
-          </div>
-          <div class="modal" id="modal">
-            <div class="modal-header">
-              <h3><span>${plant.common[0]}</span> is a great pick</h3>
-              <button data-close-modal id="closeModal">&times;</button>
-            </div>
-            <form >
-              <div class="width">
-                  <label for="name">Name</label>
-                  <input type="text" name="fullname" id="fullname" placeholder="Your name here">
-                </div>
-                <div class="width">
-                  <label for="plant type">Which plant type do  you need?</label><br>
-                  <input type="text" name="artificial/live" id="artificialOrLive" placeholder="Artificial/Live">
-                </div>
-                <div class="width">
-                  <label for="height">Approximate height needed</label>
-                  <input type="text" name="height" id="height" placeholder="In cm">
-                </div>
-                <div class="width">
-                  <label for="location">Location</label>
-                  <input type="text" placeholder="Delivery location" id="delivery">
-                </div>
-                <div class="width">
-                  <label for="contact">Telephone Number</label>
-                  <input type="tel" name="telephone" placeholder="+254..." id="telephone">
-                </div>
-                <input class="btn btn-dark" type="submit" value="Order Now">
-            </form>
-        </div>
-      <!-- overlay -->
-          <div id="overlay"></div>
-  `
-  storageForContainers.appendChild(container);
-  // remove kids of container that do not belong to the plant.category
-  let kids = storageForContainers.children;
-  removeUnnecesary(kids, plant.category)
-}
-// The external api does not have image sources, so images are fetched from the internal api
-function fetchImagesInternally(){
+// The api does not have images. The db.json stores the plants images according to their common names. The server has to be run to display images.
+function respectiveImage(){
+  let names = document.querySelectorAll("#plantName");
   fetch('http://localhost:3000/plantPics')
   .then(resp=>resp.json())
-  .then(greeneries=>greeneries.forEach(greenery=>{
-  let cards = document.querySelectorAll('.container-card.withoutDits')
-  // console.log(cards)
-  cards.forEach(card=>{
-    let title= card.querySelector('#plantName');
-    if(title.innerHTML=== greenery.name){
-      let image= title.previousElementSibling;      
-      image.src=greenery.img;
+  .then(images=>{
+    names.forEach(name=>{
+      images.forEach(image=>{
+        if(name.innerHTML===image.name){
+          let plantImage= name.previousElementSibling;
+          plantImage.src=image.img
+        }
+      })
+    })
+  })
+}
+// Upon clicking a button category, different plants pile on top of each other on the DOM even if not belonging to the category. This function removes plants not of same category.
+function removePlantsOfNotSameCategory(property){
+  let plantBoxes = document.querySelectorAll(".plantBox");
+  plantBoxes.forEach(plantBox=>{
+    if(plantBox.classList.contains(property)){
+    }else{
+      plantBox.remove();
     }
   })
-  }))
 }
-
-
-function removeUnnecesary(plants, category){
-  // console.log(kids)
-  for(let plant of plants){
-    if(plant.classList[2]!==category){
-      plant.remove()
+// One plant displayed undefined on the title (common[0]) because there was no common name given, thus a need to remove it.
+function removeUndefinedTitles(){
+  let names = document.querySelectorAll("#plantName");
+  names.forEach(name=>{
+    let parentBox= name.parentElement.parentElement.parentElement
+    if(name.innerHTML==='undefined'){
+      parentBox.remove();
     }
-    fetchImagesInternally()
-    let moreBtns=document.querySelectorAll('#moreBtn')
-    // console.log(moreBtns)
-    moreBtns.forEach(more=>{
-      more.addEventListener('click', ()=>{
-        // console.log(more.parentElement)
-        let containerCards = document.querySelectorAll('.container-card')
-        let parent1 =more.parentElement;
-        let grandparent = parent1.parentElement;
-        let title = parent1.querySelector('#plantName').innerHTML;
-        let parentSibling = parent1.nextElementSibling;
-        parent1.classList.remove('withoutDits');
-        grandparent.classList.remove('withoutDits');
-        parentSibling.classList.remove('invisible');
-        const openModalButtons = document.querySelectorAll('#openModal');
-        const closeModalButtons = document.querySelectorAll('#closeModal');
-        // console.log(closeModalButtons)
-
-
-
-        openModalButtons.forEach(btn=>{
-          btn.addEventListener('click', () => {
-            let modal = btn.parentElement.nextElementSibling;
-            let overlay = modal.nextElementSibling;
-            modal.classList.add("active");
-            overlay.classList.add("active");
-            // console.log(modal.classList)
-        })
-        })
-        closeModalButtons.forEach(btn=>{
-          btn.addEventListener('click', () => {
-            let modalChild = btn.parentElement
-            let modal = modalChild.parentElement;
-            let overlay = modal.nextElementSibling;
-            let detailsDiv =modal.previousElementSibling;
-            let containerCard = detailsDiv.previousElementSibling;
-            let parent = detailsDiv.parentElement
-
-            // console.log(modal.classList)
-            // console.log(overlay)
-
-            modal.classList.remove("active");
-            overlay.classList.remove("active")
-            detailsDiv.classList.add('invisible')
-            containerCard.classList.add('withoutDits')
-            parent.classList.add('withoutDits')
-
-        })
-        })
-
-
-
-        containerCards.forEach(card =>{
-          let titleCard = card.querySelector('#plantName').innerHTML;
-          let cardSibling = card.nextElementSibling;
-          let cardParent = card.parentElement;
-          if(cardSibling.classList.contains('invisible')){
-          }else{
-            if(title!==titleCard){
-            // console.log(title)
-            cardSibling.classList.add('invisible')
-            card.classList.add('withoutDits')
-            cardParent.classList.add('withoutDits')
-            }
-          }
-       })
-
-       let forms=document.querySelectorAll('form')
-      //  console.log(forms)
-       forms.forEach(form=>{
-        form.addEventListener('submit', (event)=>{
-          event.preventDefault();
-          // console.log('hey')
-          // console.log(event.target)
-          let formSelected = event.target
-          collectDeliveryInfo(formSelected)
-
-        })
-       })
-
-
-        })
-      })
+  })
+}
+// The api contains plants whose name (common[0]) repeat, hence multiple displays, thus need to eliminate copies.
+function findPair(){
+  let plantNames=document.querySelectorAll('#plantName')
+  for (let i = 0; i < plantNames.length; i++) {
+    for (let j = i + 1; j < plantNames.length; j++) {
+      if (plantNames[i].innerHTML === plantNames[j].innerHTML) {
+        let parentBox= plantNames[i].parentElement.parentElement.parentElement;
+        parentBox.remove();
+      }
+    }
   }
 }
-
-
-function collectDeliveryInfo(form){
-    let tel= form.querySelector('#telephone').value;
-    let delivery= form.querySelector('#delivery').value;
-    let height= form.querySelector('#height').value;
-    let fullname= form.querySelector('#fullname').value;
-    let plantType= form.querySelector('#artificialOrLive').value;
-    // console.log(tel, delivery, height, fullname, plantType)
-    let data = {
-      "name": fullname,
-      "plantType":plantType,
-      "height":height,
-      "delivery": delivery,
-      "telephoneNumber":tel,
-    }
-    // console.log(data)
-    postData(data)
+// This is centered around the plant collection with multiple events mostly involving buttons and clicks. To display or hide the modals, active class is added or removed depending on what the user anticipates.
+function moreDetails(btn){
+    let title= btn.previousElementSibling.innerHTML;
+    let box =btn.parentElement.parentElement.parentElement;
+    let modalMore=box.querySelector('.modalMore')
+    let modalInterested=box.querySelector('.modalInterested')
+    let modalOverlay=box.querySelector('#overlay');
+    // When more button is clicked, active class is added to modalMore and overlay to become visible
+    modalMore.classList.add('active');
+    modalOverlay.classList.add('active');
+    // When interested button is clicked, active class is removed from modalMore to make it invisible and added to modalInterested to make it visible
+    box.querySelector('#interestedBtn').addEventListener('click', ()=>{
+      modalMore.classList.remove('active')
+      modalInterested.classList.add('active')
+    })
+    // We want to hide all the modals and overlay when the overlay is clicked
+    modalOverlay.addEventListener('click', ()=>{
+      modalOverlay.classList.remove('active');
+      modalMore.classList.remove('active');
+      modalInterested.classList.remove('active')
+    })
+    // We also want to hide all the modals and overlay when the x button is clicked
+    box.querySelector('#closeModal').addEventListener('click', ()=>{
+      modalOverlay.classList.remove('active');
+      modalMore.classList.remove('active');
+      modalInterested.classList.remove('active')
+    })
+    // back button is in the second modal, which when clicked goes back to the first modal, hence hide second modal and display first modal
+    box.querySelector('#backBtn').addEventListener('click', ()=>{
+      modalMore.classList.add('active')
+      modalInterested.classList.remove('active')
+    })
+    // In the event a user is interested in a plant, he/she can input the details and click submit to order
+    box.querySelector('form').addEventListener('submit', (event)=>{
+      event.preventDefault();
+      collectOrderedPlantDetails(box);
+    })
 }
-// Post is not working; To resolve
+// On submission of form for plant order, data is collected and stored in an object called data
+function collectOrderedPlantDetails(box){
+  let form = box.querySelector('form');
+  let plant=box.querySelector("#plantName").innerHTML;
+  let tel= form.querySelector('#telephone').value;
+  let delivery= form.querySelector('#delivery').value;
+  let height= form.querySelector('#height').value;
+  let fullname= form.querySelector('#fullname').value;
+  let plantType= form.querySelector('#artificialOrLive').value;
+  let data = {
+    "plant":plant,
+    "name": fullname,
+    "plantType":plantType,
+    "height":height,
+    "delivery": delivery,
+    "telephoneNumber":tel,
+  }
+postData(data)
+}
+// We store the data to our internal server by fetch POST method
 function postData(data){
-
   fetch('http://localhost:3000/orders',{
     method: 'POST',
     headers:{
@@ -238,44 +230,5 @@ function postData(data){
     body:JSON.stringify(data)
   }).then(response=>response.json())
   .then(resp=>console.log(resp))
+  .catch(err=>console.error(err))
 }
-let header = document.querySelector('.header-content-main h1');
-  let headerP = document.querySelector('.header-content-main p');
-  let headerImage = document.querySelector('.header-img img');
-  let right = document.querySelector('#right');
-  let left = document.querySelector('#left');
-
-function getDataForCarousel(){
-fetch(`http://localhost:3000/startSection`)
-.then(resp=>resp.json())
-.then(responses=>{
-  displayCourosel(responses)
-
-})
-}
-function displayCourosel(responses){
-
-  if(currentItem>responses.length-1){
-    currentItem=0;
-  }else if(currentItem<0){
-currentItem=responses.length-1
-  }
-  let item = responses[currentItem]
-  header.innerHTML=item.title
-  headerP.innerHTML=item.text
-  headerImage.src=item.image
-
-}
-right.addEventListener('click', ()=>{
-
-currentItem++
-  // console.log(currentItem)
-  getDataForCarousel();
-
-
-})
-left.addEventListener('click', ()=>{
-  currentItem--
-  // console.log(currentItem)
-  getDataForCarousel();
-})
